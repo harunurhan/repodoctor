6cdc43e177c0b9c29a8cbf27d9b3cce7c56bff12
harunurhan <harunurhan17@gmail.com>
2016-03-21 20:34:39 +0200

    Change to POST

--- server/api/checkup.py
+++ server/api/checkup.py
@@ -4,15 +4,14 @@ from flask_restful import Api, Resource
 from flask import request
 from checker import Checker
 
-checkup_api = Api(Blueprint('checkup_api', __name__)) # pylint: disable=invalid-name
+checkup_api = Api(Blueprint('checkup_api', __name__))  # pylint: disable=invalid-name
 
 
 @checkup_api.resource('/checkup')
 class CheckupAPI(Resource):
     @staticmethod
-    def get():
-        #TODO: add documentation for the service including json response and post data format.
-        owner = request.args.get('owner')
-        repo = request.args.get('repo')
-        checker = Checker(owner, repo)
+    def post():
+        # TODO: add documentation for the service including json response and post data format.
+        data = request.get_json(force=True)
+        checker = Checker(data['owner'], data['repo'])
         return checker.check()
