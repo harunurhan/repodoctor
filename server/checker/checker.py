892cbbd842c6e417e5449095b6ca4dd4b3171f41
harunurhan <harunurhan17@gmail.com>
2016-04-09 23:48:18 +0300

    Refactor response key names

--- server/checker/checker.py
+++ server/checker/checker.py
@@ -3,19 +3,21 @@ from .criteria import *
 
 class Checker:
     # criteria which are checked
+    # TODO: search for an injection method which will read classes in criteria.py and inject here to be checked!
     criteria = [HaveReadmeCriterion(), HaveContributingCriterion()]
 
     def __init__(self, owner, repo):
         self.github = RepoScanner(owner, repo)
 
     def check(self):
-        errors = []
+        warnings = []
         for criterion in Checker.criteria:
             if not criterion.check(self.github):
-                errors.append({'description': criterion.description(),
+                warnings.append({'description': criterion.description(),
                                'severity': criterion.severity(),
                                'suggestion': criterion.suggestion(),
                                'guide_link': criterion.guide_link()})
-        return errors
+        # TODO: add other stuff addition to warnings
+        return {'warnings': warnings}
 
 
